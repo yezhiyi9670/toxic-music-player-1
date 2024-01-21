3306cd42c5c089a9ec47424364bcc9be15de02bf
yezhiyi9670 <yezhiyi0562@foxmail.com>
2024-01-21 15:11:14 +0800

    Update build script

--- build-util/build-zips.php
+++ build-util/build-zips.php
@@ -10,16 +10,11 @@ $is_preview = -1 != strpos($version, '-pre');
 
 $full_zip = '_build/' . $base . '-v' . $version . '-' . ($is_preview ? 'full' : 'release') . '.zip';
 print('Full zip: ' . $full_zip . "\n");
-if(file_exists($full_zip)) {
-	unlink($full_zip);
-}
 
 $update_zip = '_build/' . $base . '-v' . $version . '-' . ($is_preview ? 'update' : 'update') . '.zip';
 print('Update zip: ' . $update_zip . "\n");
-if(file_exists($update_zip)) {
-	unlink($update_zip);
-}
 
+print('Starting in 3 seconds...' . "\n");
 sleep(3);
 
 function add_file($zip, $file) {
@@ -31,10 +26,16 @@ function add_file($zip, $file) {
 	shell_exec('7z a "' . $zip . '" "' . $file . '"');
 }
 
+if(file_exists($full_zip)) {
+	unlink($full_zip);
+}
 foreach(['changelog', 'data.init', 'internal_config', 'lib', 'static', '.htaccess', 'index.php', 'LICENSE'] as $file) {
 	add_file($full_zip, $file);
 }
 
+if(file_exists($update_zip)) {
+	unlink($update_zip);
+}
 foreach(['changelog', 'lib', 'static', 'index.php'] as $file) {
 	add_file($update_zip, $file);
 }
